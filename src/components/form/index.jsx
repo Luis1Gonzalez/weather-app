fe7b6820d80eef59face5e867a19cd71ea7bfd56
luis1gonzalez <luis1gonzalez@hotmail.com>
2022-07-24 17:32:15 +0200

    ISO 3166-1

--- src/components/form/index.jsx
+++ src/components/form/index.jsx
@@ -19,7 +19,7 @@ export default function Form({newLocation}){
             <form onSubmit = {onSubmit}>
                 <div className='wrap_form col mb-3 d-flex flex-column align-items-center'>
                     <input type="text" className='input-city col w-100 text-center fs-6 rounded p-2' placeholder="Ingresa una Ciudad" required onChange={(e) =>setCity(e.target.value)} />
-                    <input type="text" className='input-country col my-3 text-center fs-6 w-100 rounded p-2' placeholder="Ingresa el País (Opcional)" maxlength="2" title="El nombre del país es opcional y debe ir en codigo ISO 3166-1 de dos letras" onChange={(e) =>setCountries(e.target.value)} />
+                    <input type="text" className='input-country col my-3 text-center fs-6 w-100 rounded p-2' placeholder="Ingresa el País (Opcional y en ISO 3166-1 de 2 letras)" maxlength="2" title="El nombre del país es opcional y debe ir en codigo ISO 3166-1 de dos letras" onChange={(e) =>setCountries(e.target.value)} />
                     <button className='btn btn-city' type="submit">Buscar</button>
                 </div>
 
