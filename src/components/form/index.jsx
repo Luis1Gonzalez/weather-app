83ca5fce554c807073b77f2e636b6a8cd67902ca
luis1gonzalez <luis1gonzalez@hotmail.com>
2022-07-24 17:26:29 +0200

    agregué el atributo title a la etiqueta del país

--- src/components/form/index.jsx
+++ src/components/form/index.jsx
@@ -18,8 +18,8 @@ export default function Form({newLocation}){
         <div className='container row d-flex'>
             <form onSubmit = {onSubmit}>
                 <div className='wrap_form col mb-3 d-flex flex-column align-items-center'>
-                    <input type="text" className='input-city col w-100 text-center fs-6 rounded p-2' placeholder="Ingresa una Ciudad" onChange={(e) =>setCity(e.target.value)} />
-                    <input type="text" className='input-country col my-3 text-center fs-6 w-100 rounded p-2' placeholder="Ingresa el País (Opcional)" onChange={(e) =>setCountries(e.target.value)} />
+                    <input type="text" className='input-city col w-100 text-center fs-6 rounded p-2' placeholder="Ingresa una Ciudad" required onChange={(e) =>setCity(e.target.value)} />
+                    <input type="text" className='input-country col my-3 text-center fs-6 w-100 rounded p-2' placeholder="Ingresa el País (Opcional en algunos casos)" maxlength="2" title="El nombre del país es opcional y debe ir en codigo ISO 3166-1 de dos letras" onChange={(e) =>setCountries(e.target.value)} />
                     <button className='btn btn-city' type="submit">Buscar</button>
                 </div>
 
